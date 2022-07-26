35326308d2f0c64bd045dea7fe7cd225c9b7ced0
suresh <suresh.kunkatla7@gmail.com>
2022-07-26 16:53:40 +0000

    update project 1

--- src/components/passwordManager/index.js
+++ src/components/passwordManager/index.js
@@ -24,35 +24,47 @@ class PasswordManager extends Component {
             </div>
             <form className="form-container">
               <h1 className="form-heading">Add New Password</h1>
-              <div>
-                <button type="button">
+              <div className="button-input-container">
+                <button type="button" className="img-button">
                   <img
                     src="https://assets.ccbp.in/frontend/react-js/password-manager-website-img.png"
                     alt="website"
                     className="input-img"
                   />
                 </button>
-                <input type="text" />
+                <input
+                  type="text"
+                  className="input-element"
+                  placeholder="Enter Website"
+                />
               </div>
-              <div>
-                <button type="button">
+              <div className="button-input-container">
+                <button type="button" className="img-button">
                   <img
                     src="https://assets.ccbp.in/frontend/react-js/password-manager-username-img.png"
                     alt="website"
                     className="input-img"
                   />
                 </button>
-                <input type="text" />
+                <input
+                  type="text"
+                  className="input-element"
+                  placeholder="Enter Username"
+                />
               </div>
-              <div>
-                <button type="button">
+              <div className="button-input-container">
+                <button type="button" className="img-button">
                   <img
                     src="https://assets.ccbp.in/frontend/react-js/password-manager-password-img.png"
                     alt="website"
                     className="input-img"
                   />
                 </button>
-                <input type="text" />
+                <input
+                  type="password"
+                  className="input-element"
+                  placeholder="Enter Password"
+                />
               </div>
               <div className="add-btn-container">
                 <button className="add-button" type="submit">
@@ -60,6 +72,37 @@ class PasswordManager extends Component {
                 </button>
               </div>
             </form>
+          </div>
+
+          <div className="password-saving-container">
+            <div className="password-saving-input-container">
+              <h1 className="your-password-text">Your Passwords</h1>
+              <div className="input-search-container">
+                <button type="button" className="search-button">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-search-img.png "
+                    alt="search"
+                    className="input-search-img"
+                  />
+                </button>
+                <input
+                  type="text"
+                  className="password-saving-input-element"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            <hr className="horizontal-line" />
+            <div className="show-password-container">
+              <input
+                id="showPassword"
+                type="checkbox"
+                className="checkbox-element"
+              />
+              <label htmlFor="showPassword" className="checkbox-label">
+                Show Password
+              </label>
+            </div>
           </div>
         </div>
       </div>
